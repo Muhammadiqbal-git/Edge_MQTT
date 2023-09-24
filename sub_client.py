13a72eb0caf0538f346cdb9a474de2fc2823f1b4
Iqbal <iniiban21@gmail.com>
2023-09-24 13:34:33 +0700

    experiment with telegram bot api http request

--- sub_client.py
+++ sub_client.py
@@ -3,6 +3,8 @@ import io
 import tensorflow as tf
 import os
 import asyncio
+import requests
+import json
 from models import decoder, prediction_head
 from utils import data_utils
 from firebase_admin import credentials, initialize_app, storage
@@ -11,7 +13,8 @@ from PIL import Image, ImageDraw
 from telegram import Bot
 from telegram.ext import MessageHandler, ContextTypes
 
-broker_address = '192.168.100.2'
+
+broker_address = '172.16.0.19'
 port = 1883
 topic = [('edge/cam/1', 0), ('edge/cam/2', 0)]
 
@@ -19,8 +22,11 @@ client_id = 'edge_1'
 
 cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS")) # add your Credentials keys path to sys environtment
 image_path = "D:\\1.Skripsi\\Edge\\img\\2023-07-28_4473.jpeg"
+image_path = os.path.join("D:\\1.Skripsi", "Edge", "img", "2023-07-28_4473.jpeg")
 with open("telegram_key.txt") as f:
     token = f.readline()
+base_url = f"https://api.telegram.org/bot{token}/sendPhoto"
+
 
 
 def model_init() -> tf.keras.Model:
@@ -35,6 +41,7 @@ def model_init() -> tf.keras.Model:
         blob = bucket.blob(blob_name=blob_name)
         blob.download_to_filename(model_path)
         print('download success')
+    print('loading the model')
     model = tf.keras.models.load_model(model_path, compile=False)
     return model
     # model_path = m_path
@@ -89,13 +96,6 @@ def infer_draw_predictions(imgs, pred_bboxes, pred_labels, pred_scores, labels):
     denormalized_bboxes = denormalize_bboxes(pred_bboxes, img_size, img_size)
     draw_bboxes_with_labels(imgs, denormalized_bboxes, pred_labels, pred_scores, labels)
 
-def send_photo_telegram(imgs, caption):
-    bot = Bot(token=token)
-    print("sending..")
-    caption = "test"
-    bot.send_message(text="asdasd")
-    bot.send_photo(photo=imgs, caption=caption)
-
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -113,11 +113,32 @@ def subscribe_mqtt(client: mqtt_client, ssd_model: tf.keras.Model, jj):
         print("Recieved data from {} topic".format(msg.topic))
         if (msg.topic == 'edge/cam/1'):
             print("Recieved from first topic")
-        print("Data : {}".format(msg.payload))
+        data_json = json.loads(msg.payload.decode())
+        print(data_json['value1'])
+        print("Data : {}".format(msg.payload.decode()))
         print("Lenght : {}".format(len(msg.payload)))
-        tes = io.BytesIO(msg.payload)
         data = data_utils.single_custom_data_gen(image_path, 300, 300)
-        send_photo_telegram(image_path, "test")
+        tes = io.BytesIO()
+        tes.name = 'tesss.jpeg'
+        image = Image.open(image_path)
+        image.save(tes, 'JPEG')
+        tes.seek(0)
+        parameter = {}
+        files = {}
+        # image = open(image_path, 'rb')
+        print('---')
+        print(type(image))
+        print(image)
+        files["photo"] = tes
+        parameter["chat_id"] = "-1001974152494"
+        # parameter["photo"] = image_path
+        
+        parameter["caption"] = msg.payload.decode()
+        print(base_url)
+        print(parameter)
+        resp = requests.post(base_url, params=parameter, files=files)
+        print(resp.status_code)
+        print(resp.content)
         p_bbox, p_scores, p_labels = ssd_model.predict(data)
         p_bbox = tf.squeeze(p_bbox)
         p_scores = tf.squeeze(p_scores)
@@ -135,4 +156,4 @@ def main():
     client.loop_forever()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
